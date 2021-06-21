d5602addbbdf81302cdaacaccbfb1f06a4a56e44
Philip Garip <phil.garip@gmail.com>
2021-06-21 19:42:21 -0400

    rework

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,12 @@
 const mysql = require("mysql");
 var connection = mysql.createConnection({
-	// Connection
 	host: "localhost",
 	port: 3306,
-	// MySQL Workbench
 	user: "root",
 	password: "Ghenna1209",
-	// Database created in schemea.sql
 	database: "employeesDB",
 });
 
-// connect to the mysql server and database
 connection.connect(function (err) {
 	if (err) throw err;
 });
