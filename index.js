cdee1a53bb2cb21a06d034e13cf435a31c9d49ce
Philip Garip <phil.garip@gmail.com>
2021-06-21 20:00:11 -0400

    layout initial prompt and make connection

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 const inquirer = require("inquirer");
 const table = require("console.table");
 const connection = require("./config/connection");
+
+
+connection.connect(function (err) {
+	if (err) throw err;
+	firstPrompt();
+})
+
+function firstPrompt() {
+  inquirer
+	.prompt({
+		type: "list",
+		message: "What would you like to do?",
+		name: "action",
+		choices: [
+			"View Employees",
+			"View Roles",
+			"View Departments",
+			"Add Employee",
+			"Add Role",
+			"Add Department",
+			"Update Employee Role",
+			"Exit"
+		]
+	})
+	.then( )
+
+}
